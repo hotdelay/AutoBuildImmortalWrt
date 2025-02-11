add2c28b120f87fda8e7d4effc0f79596a1eda2e
hotdelay <53335734+hotdelay@users.noreply.github.com>
2025-02-11 21:51:08 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -37,6 +37,8 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-app-ddns-go"
+PACKAGES="$PACKAGES luci-app-ssr-plus"
+PACKAGES="$PACKAGES luci-app-ddns"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
